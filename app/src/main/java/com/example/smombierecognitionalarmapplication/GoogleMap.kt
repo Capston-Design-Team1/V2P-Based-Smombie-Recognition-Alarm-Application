b51ce6221a81a61a6d59aaeb78c28256d55cf761
creatingweb123 <71700107+creatingweb123@users.noreply.github.com>
2024-04-22 01:50:45 +0900

    Update GoogleMap.kt

--- app/src/main/java/com/example/smombierecognitionalarmapplication/GoogleMap.kt
+++ app/src/main/java/com/example/smombierecognitionalarmapplication/GoogleMap.kt
@@ -1,91 +1,43 @@
 package com.example.smombierecognitionalarmapplication
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.viewinterop.AndroidView
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.MapView
+import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
-class GoogleMap : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MapScreen(CurrentLocationService(this))
+            MapScreenWithMarker()
         }
-    }
-}
-
-interface ILocationService {
-    fun requestCurrentLocation(): Flow<LatLng?>
-}
-
-class CurrentLocationService(private val context: Context) : ILocationService {
-    private val locationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
-
-    @SuppressLint("MissingPermission")
-    override fun requestCurrentLocation(): Flow<LatLng?> = callbackFlow {
-        if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            locationClient.lastLocation.addOnSuccessListener { location ->
-                location?.let {
-                    trySend(LatLng(it.latitude, it.longitude))
-                }
-            }
-        } else {
-            trySend(null)
-        }
-        awaitClose { }
     }
 }
 
 @Composable
-fun MapScreen(locationService: ILocationService) {
-    val context = LocalContext.current
-    val mapView = rememberMapViewWithLifecycle()
+fun MapScreenWithMarker() {
+    // change color of the marker
+    // implement onClick with popup composable
 
-    LaunchedEffect(Unit) {
-        locationService.requestCurrentLocation().collect { currentLocation ->
-            mapView.getMapAsync { googleMap ->
-                currentLocation?.let {
-                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(it, 15f))
-                    googleMap.addMarker(MarkerOptions().position(it).title("Current Location"))
-                }
-            }
-        }
+    val currentLocation = LatLng(20.302039, 134.2082661)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(currentLocation, 2f)
     }
-
-    AndroidView({ mapView }) { mapView ->
-        mapView.onResume()
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            state = MarkerState(position = currentLocation),
+            title = "Current location"
+        )
     }
 }
-
-@Composable
-fun rememberMapViewWithLifecycle(): MapView {
-    val context = LocalContext.current
-    val mapView = remember {
-        MapView(context).apply {
-            onCreate(Bundle())
-        }
-    }
-    AndroidView({ mapView }) { mapView ->
-        mapView.onResume()
-    }
-    return mapView
-}
-
-
